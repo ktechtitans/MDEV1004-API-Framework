551ec7581c3dadaa4a59a110ac9ac069eb825e8a
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-09-13 19:36:53 -0400

    Updated script.js

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
-const apiKey = 'whMJA1BtHGkIDQLnU9Mc4X6BLM1Aj4JM';
+let APIKEY = "whMJA1BtHGkIDQLnU9Mc4X6BLM1Aj4JM";
 
-function searchGIFs() {
-  const searchInput = document.getElementById('searchInput').value;
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchInput}&limit=10`;
+document.addEventListener("DOMContentLoaded", init);
 
- fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      const gifContainer = document.getElementById('gifContainer');
-      gifContainer.innerHTML = ''; 
+function init() {
+    document.getElementById("searchButton").addEventListener("click", ev => {
+        ev.preventDefault();
+        let str = document.getElementById("searchInput").value.trim();
+        let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=10&q=${str}`;
+        console.log(url);
 
-      data.data.forEach(gif => {
-        const img = document.createElement('img');
-        img.src = gif.images.fixed_height.url;
-        gifContainer.appendChild(img);
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching GIFs:', error);
+        fetch(url)
+            .then(response => response.json())
+            .then(content => {
+                console.log(content.data); 
+                console.log('META', content.meta);
+
+                let out = document.querySelector('.out');
+                out.innerHTML = ''; 
+
+                content.data.forEach(gif => {
+                    let fig = document.createElement('figure');
+                    let img = document.createElement('img');
+                    img.src = gif.images.downsized.url;
+                    fig.appendChild(img);
+                    out.insertAdjacentElement('beforeend', fig); 
+                });
+            })
+            
+            .catch(err => {
+                console.error(err);
+            });
     });
+    
 }
-
-document.getElementById('clearButton').addEventListener('click', function() {
-    document.getElementById('gifContainer').innerHTML = '';  
-    document.getElementById('searchInput').value = '';      
-  });
-
-document.getElementById('searchButton').addEventListener('click', searchGIFs);
