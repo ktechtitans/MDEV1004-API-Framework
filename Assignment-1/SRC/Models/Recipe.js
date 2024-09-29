7b45054fe77331ec10c50a726fd4fbd6714d7adc
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-09-29 02:18:30 -0400

    Updated Model

--- Assignment-1/SRC/Models/Recipe.js
+++ Assignment-1/SRC/Models/Recipe.js
@@ -1,3 +1,26 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const RecipeSchema = new mongoose.Schema({
+    recipeName: { type: String, required: true },
+    ingredients: { type: [String], required: true }, 
+    cookingTime: { type: Number, required: true },       
+    difficulty: { type: String, required: true },        
+    cuisine: { type: String, required: true },           
+    description: { type: String, required: true },       
+    photoLink: { type: String, required: true },          
+    averageRating: { type: Number, required: true }   
+});
+
+const Recipe =mongoose.model(Recipe,RecipeSchema);
+module.exports = Recipe;
+
+
+
+
+
+
+
+
+
+
