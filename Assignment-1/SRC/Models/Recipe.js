bc2e9b5e1abfc02880f0c2f1378d6e3eee57c00d
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-09-29 11:26:00 -0400

    Updated Recipe.js file

--- Assignment-1/SRC/Models/Recipe.js
+++ Assignment-1/SRC/Models/Recipe.js
@@ -1,6 +1,16 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+/**
+ * File: Recipe.js
+ * Student Name: Kanchandeep Kaur
+ * Student ID: 200603165
+ * Date: 28-09-2024
+ */
 
+const mongoose = require('mongoose');// Import Mongoose library for MongoDB interaction
+const Schema = mongoose.Schema;// Alias for mongoose.Schema
+
+/**
+ * Recipe Schema: Defines the structure of the recipe documents in the MongoDB collection.
+ */
 const RecipeSchema = new mongoose.Schema({
     recipeName: { type: String, required: true },
     ingredients: { type: [String], required: true }, 
@@ -12,8 +22,8 @@ const RecipeSchema = new mongoose.Schema({
     averageRating: { type: Number, required: true }   
 });
 
-const Recipe =mongoose.model("Recipe",RecipeSchema);
-module.exports = Recipe;
+const Recipe =mongoose.model("Recipe",RecipeSchema); // Create and export the Recipe model
+module.exports = Recipe; // Export the Recipe model for use in other parts of the application
 
 
 
