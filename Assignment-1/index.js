fe18a2ae76b66e713c9872cfbb45e12ec7b81e3f
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-09-28 23:01:31 -0400

    Added route

--- Assignment-1/index.js
+++ Assignment-1/index.js
@@ -4,6 +4,11 @@ const express = require('express');
 //Initialize the express app
 const app = express();
 
+//Define a route
+app.get('/',(req,res)=>{
+    res.send('Welcome to the first program of node js express');
+})
+
 const port = 3000;
 
 app.listen(port, ()=>{
