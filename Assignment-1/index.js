d9bb2eceba15dd8926a00b907e5c9cfd02b55d27
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-09-28 20:49:34 -0400

    Updated index.js file by setting port and starting server

--- Assignment-1/index.js
+++ Assignment-1/index.js
@@ -1,2 +1,11 @@
 //import express
 const express = require('express');
+
+//Initialize the express app
+const app = express();
+
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log('Server is running on http://localhost:${port}')
+});
