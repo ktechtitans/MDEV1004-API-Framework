7c2c24495e724d562f71dfc1dfd469555b1c7555
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-09-28 21:19:04 -0400

    fixed error

--- Assignment-1/index.js
+++ Assignment-1/index.js
@@ -7,5 +7,5 @@ const app = express();
 const port = 3000;
 
 app.listen(port, ()=>{
-    console.log('Server is running on http://localhost:${port}')
+    console.log(`Server is running on http://localhost:${port}`)
 });
