40560d843452d369e12fa769798fee568a6920bf
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-09-29 01:51:45 -0400

    Fixed error in mongo connection

--- Assignment-1/index.js
+++ Assignment-1/index.js
@@ -7,7 +7,17 @@ const mongoose = require('mongoose');
 const app = express();
 
 //MongoDb Atlas connection string
-const mongoURI = 'mongodb+srv://KaurKanchan:mongodb@cluster0.v3lhx.mongodb.net/';
+const mongoURI = 'mongodb+srv://Kanchan:Kanchan@cluster0.v3lhx.mongodb.net/';
+
+//connect to mongodb Atlas
+mongoose.connect(mongoURI,{
+})
+.then(()=>{
+    console.log('Connected to mongodb');
+})
+.catch((error)=>{
+    console.error('Error connecting to mongoDB',error);
+});
 
 //Middleware
 app.use(express.json());
