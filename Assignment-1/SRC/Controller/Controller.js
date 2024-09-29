40c467e4f6f3535823f5d01f4dd567bd4c5f54af
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-09-29 02:44:10 -0400

    Worked on post method

--- Assignment-1/SRC/Controller/Controller.js
+++ Assignment-1/SRC/Controller/Controller.js
@@ -11,3 +11,27 @@ const Recipe = async (req, res) => {
         res.status(500).json({ message: 'Failed to recieve recipes', error: error.message });
     }
 }
+const postrecipe = async (req, res) => {
+    const { recipeName, ingredients, cookingTime, difficulty, cuisine, description, photoLink, averageRating } = req.body;
+    const newRecipe = new Recipe({
+        recipeName,
+        ingredients,
+        cookingTime,
+        difficulty,
+        cuisine,
+        description,
+        photoLink,
+        averageRating
+    });
+
+    try {
+        const savedData  = await newRecipe.save();
+        res.status(201).json(savedData)
+    }
+    catch (error){
+        res.status(400).json({ message: 'Request Unsuccessfull', error: error.message });
+    }
+}
+
+
+module.exports = {Recipes, postrecipe};
