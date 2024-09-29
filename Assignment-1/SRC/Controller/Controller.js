1663f3b66c78b403f5b71a78bc7e0acc8df0a086
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-09-29 11:20:55 -0400

    Changes in Controller.js file

--- Assignment-1/SRC/Controller/Controller.js
+++ Assignment-1/SRC/Controller/Controller.js
@@ -1,19 +1,48 @@
+/**
+ * File: Controller.js
+ * Student Name: Kanchandeep Kaur
+ * Student ID: 200603165
+ * Date: 28-09-2024
+ */
+
 const recipes = require ('../Models/Recipe');
+
+/**
+ * Function: getRecipe
+ * Description: Fetches all recipes from the MongoDB collection and returns them in JSON format.
+ * Parameters: 
+ *   - req (Object): The HTTP request object.
+ *   - res (Object): The HTTP response object.
+ * Returns:
+ *   - JSON: A list of all recipes or an error message if something goes wrong.
+ */
 
 
 const getRecipe = async (req, res) => {
     try {
-        const RecipeData = await recipes.find();
+        const RecipeData = await recipes.find();  // Fetch all recipes from MongoDB collection
       
         res.status(200).json(RecipeData);
     }
     catch (error) {
-        res.status(500).json({ message: 'Failed to recieve recipes', error: error.message });
+        res.status(500).json({ message: 'Failed to recieve recipes', error: error.message }); // If any error occurs, send a 500 response with the error message
     }
 }
+
+/**
+ * Function: postRecipe
+ * Description: Adds a new recipe to the MongoDB collection.
+ * Parameters: 
+ *   - req (Object): The HTTP request object containing the recipe data in the body.
+ *   - res (Object): The HTTP response object.
+ * Returns:
+ *   - JSON: The saved recipe data or an error message if the operation fails.
+ */
+
+
 const postrecipe = async (req, res) => {
     const { recipeName, ingredients, cookingTime, difficulty, cuisine, description, photoLink, averageRating } = req.body;
-    const newRecipe = new recipes({
+    const newRecipe = new recipes({      // Create a new Recipe instance using the request body data
         recipeName,
         ingredients,
         cookingTime,
@@ -26,12 +55,12 @@ const postrecipe = async (req, res) => {
 
     try {
         const savedData  = await newRecipe.save();
-        res.status(201).json(savedData)
+        res.status(201).json(savedData)   // Return the saved recipe data in JSON format with a 201 status code (created successfully)
     }
     catch (error){
         res.status(400).json({ message: 'Request Unsuccessfull', error: error.message });
     }
 }
 
-
+// Export the controller functions for use in the routes
 module.exports = {getRecipe, postrecipe};
