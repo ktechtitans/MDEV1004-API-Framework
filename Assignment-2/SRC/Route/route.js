df6259cf060c0eaeaf97c808b0bc674553a7e9fc
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-10-13 02:34:18 -0400

    Added route.js file

---
+++ Assignment-2/SRC/Route/route.js
@@ -0,0 +1,10 @@
+const express  = require('express');
+const router = express.Router();
+
+const {getRecipe, postrecipe } = require('../Controller/Controller');
+
+router.get('/getall', getRecipe);
+
+router.post('/createNew', postrecipe);
+
+module.exports = router;
