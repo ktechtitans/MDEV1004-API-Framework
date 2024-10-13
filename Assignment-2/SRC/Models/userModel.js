97c0863bb8a842cad04f2d30afd4e846d33720a7
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-10-13 19:35:01 -0400

    Implement user login functionality

--- Assignment-2/SRC/Models/userModel.js
+++ Assignment-2/SRC/Models/userModel.js
@@ -24,3 +24,28 @@ const registerUser = async (req, res) => {
     res.status(500).send("Error occured");
   }
 };
+
+const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+      if (!email || !password) {
+        res.status(401).json({ message: "All fields are required" });
+        return;
+      }
+  
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+  
+      const isPasswordValid = await user.isValidPassword(password);
+      if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password" });
+      }
+      req.session.userId = user._id;
+      res.status(200).json({ message: "Login successful", userId: user._id });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send("Error occured");
+    }
+  };
