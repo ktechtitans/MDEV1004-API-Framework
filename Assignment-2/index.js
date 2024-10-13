55b3f61a434f17fb48a6d78d70d3e3c345ca8039
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-10-13 02:10:19 -0400

    Imported libraries and modules

--- Assignment-2/index.js
+++ Assignment-2/index.js
@@ -2,3 +2,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+//Initialize the express app
+const app = express();
+
+//MongoDb Atlas connection string
+const mongoURI = 'mongodb+srv://Kanchan:Kanchan@cluster0.v3lhx.mongodb.net/';
+
+//connect to mongodb Atlas
+mongoose.connect(mongoURI,{
+})
+.then(()=>{
+    console.log('Connected to mongodb');
+})
+.catch((error)=>{
+    console.error('Error connecting to mongoDB',error);
+});
