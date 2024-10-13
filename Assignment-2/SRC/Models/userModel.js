9bb60b782a4e0a8f4921c03e5c75d86478bc0f9f
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-10-13 19:47:37 -0400

    Created User model with username, email, and password fields in usermodel.js file

--- Assignment-2/SRC/Models/userModel.js
+++ Assignment-2/SRC/Models/userModel.js
@@ -1,3 +1,12 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 
+//Creating modes
+const UserSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  });
+  
+  
+ 
