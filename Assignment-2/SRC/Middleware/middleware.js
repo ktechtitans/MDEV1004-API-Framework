1100683dcaa33503afd4e51e4fcf4bb101aa2874
Kanchandeep Kaur <romalkanchandeepkaur@gmail.com>
2024-10-13 20:18:00 -0400

    Created middleware.js file

---
+++ Assignment-2/SRC/Middleware/middleware.js
@@ -0,0 +1,31 @@
+const validateRecipe = (req, res, next) => {
+    const {
+      recipeName,
+      ingredients,
+      cookingTime,
+      difficulty,
+      cuisine,
+      description,
+      photoLink,
+      averageRating,
+    } = req.body;
+  
+    // Check for missing or empty fields
+    if (
+      !recipeName ||
+      !ingredients ||
+      !cookingTime ||
+      !difficulty ||
+      !cuisine ||
+      !description ||
+      !averageRating
+    ) {
+      return res
+        .status(400)
+        .send(
+          "Missing any of the required fileds recipeName, ingredients, cookingTime,difficulty, cuisine, description,photoLink,averageRating"
+        );
+    }
+  
+    next();
+  };
